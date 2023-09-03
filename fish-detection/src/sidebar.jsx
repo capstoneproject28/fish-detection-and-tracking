c3c79ea0bcda702cb97503a26114061e05078b76
joshmcevoy <82429015+joshmcevoy@users.noreply.github.com>
2023-09-03 20:03:52 +1000

    updated logo

--- fish-detection/src/sidebar.jsx
+++ fish-detection/src/sidebar.jsx
@@ -6,7 +6,7 @@ function Sidebar() {
 		<div className="sidebar">
 			<ul className="sidebarItems">
 				<li className="sidebarItem">
-					<img style={{ display: 'block', margin: '0 auto' }} width="60px" height="60px" src="./src/assets/placeholderlogo.png" />
+					<img style={{ display: 'block', margin: '0 auto' }} width="60px" height="60px" src="./src/assets/logoMin.png" />
 				</li>
 				<li className="sidebarItem">
 					<img style={{ display: 'block', margin: '0 auto' }} src="./src/assets/analytics.svg" width="30" height="30" />
