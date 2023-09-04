af9585713d864c0f0319608f97b331e250904e20
Adnan <adnanzafar2001@gmail.com>
2023-09-04 16:29:58 +1000

    removed existing code

--- backend/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from frontend.views import index
-
-urlpatterns = [
-    path('', index),
-    path('join', index),
-    path('create', index),
-    path('join/1', index)
-]
