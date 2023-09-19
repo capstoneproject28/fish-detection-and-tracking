1f71dfd274b2af5d2d7ba5268e9dfee7d8503f44
RohanSoni-02 <soni.rohanchd@gmail.com>
2023-09-19 20:37:50 +1000

    added signin/register transition page

--- fish-detection/src/main.jsx
+++ fish-detection/src/main.jsx
@@ -5,15 +5,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './components/App.jsx'
 import Register from './components/RegisterScreen.jsx'
 import './index.css'
+import Upload from './Upload.jsx';
+import Sidebar from './components/sidebar.jsx';
+import SignInSignUp from './SignInSignUp.jsx';
+import RegisterScreen from './components/RegisterScreen.jsx';
 
 export default function Main() {
   return (
+    <>
+    <SignInSignUp />
+
+    </>
+    /*
     <BrowserRouter>
       <Routes>
         <Route path="/upload" element={ <App /> } />
         <Route path="/register" element={ <Register /> } />
       </Routes>
     </BrowserRouter>
+    */
   );
 }
 
