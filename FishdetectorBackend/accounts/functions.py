fc8d8c16f9bfac36e6070d1eed2cd3e57ef26b57
Muhammad Abubakar <>
2023-09-22 17:49:10 +1000

    unit tests, pep8 styling format and superuser added

--- FishdetectorBackend/accounts/functions.py
+++ FishdetectorBackend/accounts/functions.py
@@ -6,23 +6,26 @@ import firebase_admin
 import json
 
 cred = credentials.Certificate("./serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {"storageBucket": "project28-f22ba.appspot.com"})
+firebase_admin.initialize_app(
+    cred, {"storageBucket": "project28-f22ba.appspot.com"})
 
-def handle_uploaded_file(f): 
+
+def handle_uploaded_file(f):
     upload_dir = './upload/'
-    os.makedirs(upload_dir, exist_ok=True) 
-    
+    os.makedirs(upload_dir, exist_ok=True)
+
     # Add a timestamp to the original filename
     timestamp = int(time.time())  # Get current timestamp
     unique_filename = f"{timestamp}_{f.name}"
-    
+
     file_path = os.path.join(upload_dir, unique_filename)
-    
-    with open(file_path, 'wb+') as destination:  
-        for chunk in f.chunks():  
+
+    with open(file_path, 'wb+') as destination:
+        for chunk in f.chunks():
             destination.write(chunk)
-    
+
     return file_path, unique_filename  # Return the new unique filename
+
 
 def convert_avi_to_mp4(input_path, output_path):
     try:
@@ -31,6 +34,7 @@ def convert_avi_to_mp4(input_path, output_path):
         print("Conversion successful.")
     except Exception as e:
         print("Conversion failed:", str(e))
+
 
 """ def upload_video_to_firebase(video_path, destination_path):
     bucket = storage.bucket()
@@ -41,16 +45,16 @@ def convert_avi_to_mp4(input_path, output_path):
 
     return public_url """
 
+
 def upload_video_to_firebase(video_path, destination_path, result_name):
     bucket = storage.bucket()
     blob = bucket.blob(destination_path)
-    json_data = json.dumps(result_name, ensure_ascii=False)    
+    json_data = json.dumps(result_name, ensure_ascii=False)
     # Set the metadata to include the result_name
     blob.metadata = {"result_name": json_data}
-    
+
     blob.upload_from_filename(video_path)
     blob.make_public()
     public_url = blob.public_url
 
     return public_url
-
--- FishdetectorBackend/accounts/tests.py
+++ FishdetectorBackend/accounts/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import patch, mock_open, MagicMock
+import time
+from functions import handle_uploaded_file, convert_avi_to_mp4, upload_video_to_firebase
 
-# Create your tests here.
+
+class TestVideoProcessing(unittest.TestCase):
+
+    def test_handle_uploaded_file(self):
+        # Mock the file object
+        mock_file = MagicMock()
+        mock_file.name = "test.avi"
+        mock_file.chunks.return_value = [b"chunk1", b"chunk2"]
+
+        # Mock open method
+        with patch("builtins.open", mock_open()) as mocked_open:
+            file_path, unique_filename = handle_uploaded_file(mock_file)
+
+        # Check if unique filename is generated correctly
+        self.assertTrue(unique_filename.startswith(
+            f"{int(time.time())}_test.avi"))
+
+        # Check if file is written correctly
+        mocked_open.assert_called_with(f"./upload/{unique_filename}", "wb+")
+        handle = mocked_open()
+        handle.write.assert_any_call(b"chunk1")
+        handle.write.assert_any_call(b"chunk2")
+
+    @patch('functions.VideoFileClip')
+    @patch('functions.print')
+    def test_convert_avi_to_mp4(self, mock_print, mock_videofileclip):
+        # Mock VideoFileClip's behavior
+        mock_clip = MagicMock()
+        mock_videofileclip.return_value = mock_clip
+
+        convert_avi_to_mp4("input.avi", "output.mp4")
+
+        mock_videofileclip.assert_called_with("input.avi")
+        mock_clip.write_videofile.assert_called_with(
+            "output.mp4", codec="libx264")
+        mock_print.assert_called_with("Conversion successful.")
+
+
+if __name__ == '__main__':
+    unittest.main()
