cb6aa7c48ce3e3775b28a1234d9d4cf5e14df861
RohanSoni-02 <soni.rohanchd@gmail.com>
2023-09-29 20:19:01 +1000

    changed css

--- fish-detection/src/App.jsx
+++ fish-detection/src/App.jsx
@@ -7,6 +7,7 @@ export default function App(){
     return(
         <>
         <Canvas
+            style={{ width: '100%', height: '100%' }}
             shadows
             camera={ {
                 fov: 45,
