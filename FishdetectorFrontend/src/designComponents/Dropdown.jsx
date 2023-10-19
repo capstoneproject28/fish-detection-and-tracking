5db7b70a82b315b37b658e440cb061975966a1fa
Adnan <adnanzafar2001@gmail.com>
2023-10-20 01:00:49 +1100

    fixed dropdown menu issue and improved graphs

--- FishdetectorFrontend/src/designComponents/Dropdown.jsx
+++ FishdetectorFrontend/src/designComponents/Dropdown.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import '../css/dropdown.css'
+import React, { useState } from 'react';
+import '../css/dropdown.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-export default function DropDown () {
+export default function DropDown() {
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState('YOLO'); // Default to CNN
+  const [isOpen, setIsOpen] = useState(false);
+
+  const algorithms = ['YOLO', 'CNN', 'RNN', 'RCNN'];
+
+  const handleSelection = (algorithm) => {
+    setSelectedAlgorithm(algorithm);
+    setIsOpen(false); // Close the dropdown
+  };
+
+  const toggleDropdown = (event) => {
+    event.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   return (
-
-      <div>
-  <legend>Dropdown</legend>
-  <details open="">
-    <summary>
-      Select your Machine Learning Algorithm 
-      <i className="fa fa-caret-down" aria-hidden="true"></i>
-    </summary>
     <div>
-      <label>
-        <input type="radio" name="radio" />
-        <span>Yolo</span>
-      </label>
-      <label>
-        <input type="radio" name="radio" defaultChecked="" />
-        <span>CNN</span>
-      </label>
-      <label>
-        <input type="radio" name="radio" />
-        <span>RNN</span>
-      </label>
-      <label>
-        <input type="radio" name="radio" />
-        <span>RCNN</span>
-      </label>
-      <div className="fade" />
+      <legend>Dropdown</legend>
+      <details open={isOpen}>
+        <summary onClick={toggleDropdown}>
+          Select your Machine Learning Algorithm: {selectedAlgorithm} 
+          <i className="fa fa-caret-down" aria-hidden="true"></i>
+        </summary>
+        <div>
+          {algorithms.map((algorithm) => (
+            <label key={algorithm}>
+              <input 
+                type="radio" 
+                name="radio" 
+                checked={selectedAlgorithm === algorithm}
+                onChange={() => handleSelection(algorithm)}
+              />
+              <span>{algorithm}</span>
+            </label>
+          ))}
+          <div className="fade" />
+        </div>
+      </details>
     </div>
-  </details>
-</div>
-
-    
-  )
+  );
 }
